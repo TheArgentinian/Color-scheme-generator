8176650a3d02183944f551ba268dea8f85f35533
Federico <fede4488@gmail.com>
2023-11-07 19:01:07 -0300

    onclick

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const colorSelector = document.getElementById("color-sel")
 const schemeSelector = document.getElementById("scheme-selector")
 const colorsRender = document.getElementById("colors-render")
 
-
 function getColors(){
      fetch(`"https://www.thecolorapi.com/scheme?hex=${colorSelector.value}&${schemeSelector}`)
     .then(response => response.json())
     .then(data => {
-        renderColors(data.colors)
+        //renderColors(data.colors)
+        console.log(data)
     })
 }
 
 function renderColors (colorsArray){
-    colorsRender.innerHTML = colorsArray.map(color => { //bracket innecesaria?//
+    colorsRender.innerHTML = colorsArray.map(color => { 
         return `<div class="colors" 
                 style="background-color: ${color.hex.value}">
                 <div class="color-name">${color.name.value}</div>
@@ -20,3 +20,4 @@ function renderColors (colorsArray){
         `
     })
 }
+
