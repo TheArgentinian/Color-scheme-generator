34b8fd5cfcc56bed073d9b76e1cd08cb5b1e231d
Federico <fede4488@gmail.com>
2024-06-14 22:19:36 -0300

    trabajo en botón de colores

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const colorSelector = document.getElementById("color-sel")
 const schemeSelector = document.getElementById("scheme-selector")
 const colorsRender = document.getElementById("colors-render")
+const schemeButton = document.getElementById('button-scheme')
+
+console.log(colorSelector.value)
+
+colorSelector.addEventListener("click", getColors)
 
 function getColors(){
-     fetch(`"https://www.thecolorapi.com/scheme?hex=${colorSelector.value}&${schemeSelector}`)
+     fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector.value}`)
     .then(response => response.json())
     .then(data => {
         //renderColors(data.colors)
         console.log(data)
     })
 }
+
 
 function renderColors (colorsArray){
     colorsRender.innerHTML = colorsArray.map(color => { 
