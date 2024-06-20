96ec677453a7678af9318e5a21685ae254c9a97c
Federico <fede4488@gmail.com>
2024-06-20 19:59:14 -0300

    selector de esquemas funcionando

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const colorSelector = document.getElementById("color-sel")
 const schemeSelector = document.getElementById("scheme-selector")
 const colorsRender = document.getElementById("colors-render")
 const schemeButton = document.getElementById('scheme-button')
+const test = document.getElementById("test")
+let schemes = []
+getColors()
 
-schemeSelector.addEventListener("click", getColors)
+schemeButton.addEventListener("click", getColors)
 
 function getColors(){
      fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector.value.substr(-6)}&mode=${schemeSelector.value}&count=5`)
     .then(response => response.json())
     .then(data => {
         renderColors(data.colors)
+        schemes = data._links.schemes
+        getSchemeColors()
     })
 }
 
 
-function renderColors (colorsArray){
+function renderColors(colorsArray){
     colorsRender.innerHTML = colorsArray.map(color => { 
         return `<div class="colors" 
                 style="background-color: ${color.hex.value}">
@@ -22,5 +27,14 @@ function renderColors (colorsArray){
             </div>
         `
     })
+}
+
+function getSchemeColors(){
+    Object.entries(schemes).map(([key, value]) => (            
+        schemeSelector.innerHTML += `
+        <option value=${value}>${key}</option>
+        `
+    )
+)
 }
 
